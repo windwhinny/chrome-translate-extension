8f17d88ff8cb7c7840724d5bc7f0d0a2562209de
时允 <shiyun.01@bytedance.com>
2025-03-25 12:01:40 +0800

    test: 添加 JSONString 解析测试用例
    在 JSON 测试文件中新增了 JSONString 的解析测试用例，用于验证转义字符的解析功能

--- src/background/lib/json.test.ts
+++ src/background/lib/json.test.ts
@@ -1,26 +1,41 @@
 import { describe, it, expect } from "vitest";
-import { JSONNumber } from "./json";
+import { JSONNumber, JSONString } from "./json";
 import { makeGenerator } from "./lib.test";
 import { StreamReader } from "./reader";
 
-describe('JSONNumber', () => {
-  const cases = ['123', '.3', '123.456', '-123.456', '+123.456', '-32.', '123e-456', '123e+456', '123e-1', '123e+3'];
-  it('should parse number', async () => {
-    for (let str of cases) {
-      const generator = makeGenerator(str);
-      const num = new JSONNumber('');
-      await num.parse(new StreamReader(generator()));
-      expect(num.str).toBe(str);
-    }
+describe('JSON', () => {
+  describe('JSONNumber', () => {
+    const cases = ['123', '.3', '123.456', '-123.456', '+123.456', '-32.', '123e-456', '123e+456', '123e-1', '123e+3'];
+    it('should parse number', async () => {
+      for (let str of cases) {
+        const generator = makeGenerator(str);
+        const num = new JSONNumber('');
+        await num.parse(new StreamReader(generator()));
+        expect(num.str).toBe(str);
+      }
+    });
+  
+    const faildCases = ['123e', '123e-', '123e+', '123e-1-', '123e+1+', '12-', '.-'];
+    it('should faild', async () => {
+      for (let str of faildCases) {
+        const generator = makeGenerator(str);
+        const num = new JSONNumber('');
+        const result = num.parse(new StreamReader(generator()));
+        expect(result).rejects.toThrow();
+      }
+    });
   });
 
-  const faildCases = ['123e', '123e-', '123e+', '123e-1-', '123e+1+', '12-', '.-'];
-  it('should faild', async () => {
-    for (let str of faildCases) {
-      const generator = makeGenerator(str);
-      const num = new JSONNumber('');
-      const result = num.parse(new StreamReader(generator()));
-      expect(result).rejects.toThrow();
-    }
-  });
-});
+  describe('JSONString', () => {
+    const cases = ['"12313\\n123123"'];
+    it('should parse escape', async () => {
+      for (let str of cases) {
+        const first = str[0];
+        const generator = makeGenerator(str.substring(1, str.length));
+        const parser = new JSONString(first);
+        await parser.parse(new StreamReader(generator()));
+        expect(parser.value).toBe('12313\n123123')
+      }
+    }); 
+  })
+})
